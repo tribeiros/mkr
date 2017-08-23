cb9c62cb2f3edbab407c33d92a9a6e7b3b0e8887
tribeiros <tribeiros@indracompany.com>
2017-08-23 15:14:19 -0300

    First commint

---
+++ mkr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Script to create a repository on github account
+#tribeiros@indracompany.com
+
+#Variables 
+USER=$1 #user name
+REPO=$2 #repository name
+
+# Validating user arguments
+  if [ ! $2 ]; then
+    echo "Repository name is missing "
+  	exit
+  fi
+
+#exec
+curl -u "${USER}" https://api.github.com/user/repos -d '{"name":"'${REPO}'"}'
+
